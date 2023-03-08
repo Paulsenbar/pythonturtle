f5c1749f88293442509592c9c2f1c95b7fa85448
Paulsenbar <113934872+Paulsenbar@users.noreply.github.com>
2023-03-08 14:12:26 -0500

    Create redcross.py

---
+++ redcross.py
@@ -0,0 +1,34 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.color('red')
+t.begin_fill()
+
+
+
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(270)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(-90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(-90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+
+t.end_fill()
